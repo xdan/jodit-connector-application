766748f7237686e512deeda47fef957a381b3bcf
Chupurnov Valeriy <skoder@ya.ru>
2018-02-28 20:22:24 +0500

    Change license

--- src/BaseApplication.php
+++ src/BaseApplication.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package    jodit
+ *
+ * @author     Valeriy Chupurnov <chupurnov@gmail.com>
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ * @link       https://xdsoft.net/jodit/
+ */
 namespace Jodit;
 
 use abeautifulsite\SimpleImage;
--- src/Helper.php
+++ src/Helper.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @package    jodit
+ *
+ * @author     Valeriy Chupurnov <chupurnov@gmail.com>
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ * @link       https://xdsoft.net/jodit/
+ */
+
 namespace Jodit;
 
 abstract class Helper {
